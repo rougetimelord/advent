794137b8b4b8293d8f52e1d2eb1309d1444f2f1c
Rouge <14047458+rougetimelord@users.noreply.github.com>
2023-12-12 21:07:38 -0800

    Set up 11

---
+++ 11/main.mjs
@@ -0,0 +1,39 @@
+/**
+ * @typedef Point
+ * @property {number} x
+ * @property {number} y
+ */
+/**
+ * @typedef {{string: Point}} StarMap
+ */
+
+/**
+ * @param {string} s 
+ */
+export const main = (s) => {
+}
+
+/**
+ * @todo expand all empty rows and columns by duplicating them
+ * 
+ * @param {string} s
+ * @returns {string} 
+ */
+const expansion = (s) => {
+}
+
+/**
+ * @param {string} s
+ * @returns {StarMap}
+ */
+const createMap = (s) =>{
+}
+
+/**
+ * @todo measure the distance between two points only moving UDLR
+ * @param {Point} p1 
+ * @param {Point} p2
+ * @returns {number}
+ */
+const distance = (p1, p2) => {
+}
