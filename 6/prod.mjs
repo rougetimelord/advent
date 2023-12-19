2051ac54c24622265fdc7af0ef3cba88dc94b174
Rouge <14047458+rougetimelord@users.noreply.github.com>
2023-12-18 21:16:34 -0800

    Remove real inputs

--- 6/prod.mjs
+++
@@ -1,7 +0,0 @@
-import { part1, part2 } from "./1.mjs"
-
-const data = `Time:        40     82     91     66
-Distance:   277   1338   1349   1063`
-
-console.log("res: ", part1(data));
-console.log("res: ", part2(data));
