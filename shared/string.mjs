f13535f60b38b9e74ea7fa70b2d4f36a05036a1f
Rouge <14047458+rougetimelord@users.noreply.github.com>
2024-01-16 16:47:40 -0800

    Split on CRLF too, add splitting on empty lines

--- shared/string.mjs
+++ shared/string.mjs
@@ -2,10 +2,12 @@
  * @param {String} s 
  * @returns {String[]}
  */
-export const str2Lines = (s) => s.split("\n")
+export const str2Lines = (s) => s.split(/\r?\n/)
 
 /**
  * @param {string} s 
  * @returns String stripped of all whitespace
  */
 export const stripWhiteSpace = (s) => s.replace(/\s/g, "")
+
+export const splitEmptyLines = (s) => s.split(/\r?\n\r?\n/)
