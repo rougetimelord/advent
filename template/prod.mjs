c161f86cb0fd844edca4f16ccee74f67adbbabb0
Rouge <14047458+rougetimelord@users.noreply.github.com>
2024-01-16 16:19:28 -0800

    Update templates to load from text file

--- template/prod.mjs
+++
@@ -1,6 +0,0 @@
-import { log } from "../../shared/logger.mjs"
-import { main } from "./main.mjs";
-
-const data = ``;
-
-log(main(data))
--- template/test.mjs
+++
@@ -1,7 +0,0 @@
-import { log } from "../../shared/logger.mjs"
-import { main } from "./main.mjs";
-
-const ans = 0 /**@todo fill in sample answer */;
-const data = ``;
-
-log(main(data), ans)
