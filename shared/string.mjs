b5530f0f9ab26f39f34a1fca63289470207fcc61
Rouge <14047458+rougetimelord@users.noreply.github.com>
2024-01-16 18:44:51 -0800

    Add ASCII priority function

--- shared/string.mjs
+++ shared/string.mjs
@@ -10,4 +10,35 @@ export const str2Lines = (s) => s.split(/\r?\n/)
  */
 export const stripWhiteSpace = (s) => s.replace(/\s/g, "")
 
+/**
+ * @param {string} s 
+ * @returns 
+ */
 export const splitEmptyLines = (s) => s.split(/\r?\n\r?\n/)
+
+/**
+ * @param {string} s 
+ * @returns 
+ */
+export const splitInHalf = (s) => [s.slice(0, s.length / 2), s.slice(s.length / 2)]
+
+/**
+ * @param  {...string} s 
+ * @returns 
+ */
+export const findCommon = (...s) => {
+    const searches = Array.from(new Set(s[0].split("")));
+    return searches.reduce((p, v) => {
+        if (s[1].includes(v)) p.push(v);
+        return p;
+    }, [])
+}
+
+/**
+ * @param {string} s
+ * @returns
+ */
+export const priority = (s) => {
+    const code = s.charCodeAt(0);
+    return (code > 96) ? code - 96 : code - 38;
+}
